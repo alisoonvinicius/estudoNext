774af413bf9b13a63e8e6855c0ba699d50d93457
alisoonvinicius <alisoonv12@gmail.com>
2021-11-29 15:53:12 -0300

    Teste

---
+++ teste/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>
+        gsdgethefgergbr
+
+      </h1>
+      <h2>
+        afsasfadgadgda
+
+      </h2>
+
+    </div>
+  )
+}
